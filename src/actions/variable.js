fb17e785fd6115dbe93ac580843cdb34e6cfff54
Shophree <udayveerus348567@gmail.com>
2023-06-27 09:36:08 +0530

    uncommend base_url

--- src/actions/variable.js
+++ src/actions/variable.js
@@ -1,6 +1,6 @@
 import { CHANGE_VARIABLE } from "./type"
 // export const base_url = "http://localhost:3022"
-// export const base_url = "https://mern-app-api-o0hg.onrender.com"
+export const base_url = "https://mern-app-api-o0hg.onrender.com"
 export const changeVariable = (key, value) => {
 	
     return ({
